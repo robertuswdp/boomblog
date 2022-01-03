5fd0e10329763a781b6c7cace5128ec3b4d4a380
robertuswdp <robertuswdp.com>
2022-01-03 15:35:28 +0700

    Model Collect Controller DONE BOSSQUE

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-danger">
     <div class="container">
-      <a class="navbar-brand" href="/">WPU Blog</a>
+      <a class="navbar-brand" href="/">BOOM Blog</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -13,7 +13,7 @@
             <a class="nav-link  {{ ($tittle === "About") ? 'active' : '' }}" href="/about">About</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link {{ ($tittle === "Posts") ? 'active' : '' }}" href="/blog">Blog</a>
+            <a class="nav-link {{ ($tittle === "Posts") ? 'active' : '' }}" href="/posts">Blog</a>
           </li>
         </ul>
       </div>
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -8,6 +8,6 @@
         <p>{{ $post["body"] }}</p>
     </article>
 
-    <a href="/blog">Back to Posts</a>
+    <a href="/posts">Back to Posts</a>
 
 @endsection
